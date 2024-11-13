4945dced7893f2a8de113c14e54f3014538482db
Pedro Henrique <pedro.phoa1405@gmail.com>
2024-11-12 21:00:26 -0300

    Iniciando e configurando projeto

---
+++ sunny-meter/sunny-meter/src/main/java/br/com/fiap/sunnymeter/sunny_meter/SunnyMeterApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.sunnymeter.sunny_meter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunnyMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunnyMeterApplication.class, args);
+	}
+
+}
---
+++ sunny-meter/sunny-meter/src/test/java/br/com/fiap/sunnymeter/sunny_meter/SunnyMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.sunnymeter.sunny_meter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunnyMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
